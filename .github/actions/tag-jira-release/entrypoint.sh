670b2ca2673708425a54cd8298dd3f450d4141da
Ansel Robateau <ansel.robateau@redshelf.com>
2024-11-14 01:14:01 -0600

    chore: remove debugging code

--- .github/actions/tag-jira-release/entrypoint.sh
+++ .github/actions/tag-jira-release/entrypoint.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Pass the arguments to the Node.js application
 if [ "$DRY_RUN" == "true" ]; then
-  bash -xv $SCRIPT_DIR/jira_tag_tickets.sh --dry-run
+  $SCRIPT_DIR/jira_tag_tickets.sh --dry-run
 else
-  bash -xv $SCRIPT_DIR/jira_tag_tickets.sh
+  $SCRIPT_DIR/jira_tag_tickets.sh
 fi
