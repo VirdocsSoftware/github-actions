4cf4e6f5c2ae522402062d6a1361b3f25d3b1c1c
nastya-scherenkova-redshelf <108456718+nastya-scherenkova-redshelf@users.noreply.github.com>
2024-12-13 08:00:54 +0300

    feat: 🎸 gitHub actions workflow job to validate PR (#54)
    * feat: 🎸 gitHub actions workflow job to validate PR
    
    ✅ Closes: CORE-1611
    
    * test: 💍 add unit tests
    
    ✅ Closes: CORE-1611
    
    * feat: 🎸 pr title must contain a jira ticket id
    
    ✅ Closes: CORE-1611
    
    * feat: 🎸 a jira ticket with a semantic prefix and ticket id is valid
    
    ✅ Closes: CORE-1611
    
    * feat: 🎸 fail on invalid release branch
    
    ✅ Closes: CORE-1611
    
    * feat: 🎸 recognize mismatch between package.json and branch name
    
    ✅ Closes: CORE-1611
    
    * feat: 🎸 use bash
    
    ✅ Closes: CORE-1611
    
    * test: 💍 validate golden path
    
    ✅ Closes: CORE-1611
    
    * feat: 🎸 check for release version greater than latest version
    
    ✅ Closes: CORE-1611
    
    * feat: 🎸 check status when targeting release or hotfix branch
    
    ✅ Closes: CORE-1611
    
    * feat: 🎸 add passed output
    
    ✅ Closes: CORE-1611
    
    * ci: 🎡 use the validate-pr shared actino
    
    ✅ Closes: CORE-1611
    
    * ci: 🎡 remove input parameter
    
    ✅ Closes: CORE-1611
    
    * ci: 🎡 use composite action
    
    ✅ Closes: CORE-1611
    
    * test: 💍 print all properties
    
    ✅ Closes: CORE-1611
    
    * test: 💍 print all variables
    
    ✅ Closes: CORE-1611
    
    * test: 💍 print all inputs
    
    ✅ Closes: CORE-1611
    
    * ci: 🎡 change variables
    
    ✅ Closes: CORE-1611
    
    * ci: 🎡 print the inputs
    
    ✅ Closes: CORE-1611
    
    * ci: 🎡 use the inputs object
    
    ✅ Closes: CORE-1611
    
    * feat: 🎸 support merging releases back to develop
    
    ✅ Closes: CORE-1611
    
    * ci: 🎡 get action from main branch
    
    ✅ Closes: CORE-1611
    
    ---------
    
    Co-authored-by: Ansel Robateau <ansel.robateau@redshelf.com>

---
+++ .github/actions/validate-pr/validate_pr.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+if [ "$PR_TITLE" == "" ]; then
+  echo "env variable PR_TITLE is required"
+  exit 1
+fi
+if [ "$PR_BRANCH" == "" ]; then
+  echo "env variable PR_BRANCH is required"
+  exit 1
+fi
+if [ "$LATEST_RELEASE" == "" ]; then
+  echo "env variable LATEST_RELEASE is required"
+  exit 1
+fi
+if [ "$PACKAGE_VERSION" == "" ]; then
+  echo "env variable PACKAGE_VERSION is required"
+  exit 1
+fi
+if [ "$TARGET_BRANCH" == "" ]; then
+  echo "env variable TARGET_BRANCH is required"
+  exit 1
+fi
+
+SEMANTIC_PREFIXES="^(feat|fix|chore|docs|style|refactor|perf|test):"
+JIRA_TICKET="([A-Z]+-[0-9]+)"
+VERSION_REGEX="^v([0-9]+)\.([0-9]+)\.([0-9]+)$"
+
+if [[ "$TARGET_BRANCH" == "develop" ]] || [[ "$TARGET_BRANCH" =~ ^release/v ]] || [[ "$TARGET_BRANCH" =~ ^hotfix/v ]]; then
+  if [[ "$PR_BRANCH" =~ ^release/v ]] || [[ "$PR_BRANCH" =~ ^hotfix/v ]]; then
+    echo "PR title and branch name validation passed."
+    exit 0
+  fi
+  if [[ ! "$PR_TITLE" =~ $SEMANTIC_PREFIXES ]]; then
+    echo "PR title must start with a valid semantic prefix (e.g., feat:, fix:)."
+    exit 1
+  fi
+
+  if [[ ! "$PR_TITLE" =~ $JIRA_TICKET ]]; then
+    echo "PR title must contain a valid Jira ticket ID (e.g., ABC-123)."
+    exit 1
+  fi
+fi
+
+if [[ "$TARGET_BRANCH" == "main" ]]; then
+  if [[ "$PR_BRANCH" =~ ^release/v ]] || [[ "$PR_BRANCH" =~ ^hotfix/v ]]; then
+    if [[ ! "$PR_BRANCH" =~ ^release/v$PACKAGE_VERSION ]] && [[ ! "$PR_BRANCH" =~ ^hotfix/v$PACKAGE_VERSION ]]; then
+      echo "PR branch and package version must match"
+      exit 1
+    elif [[ "$(printf '%s\n' "$PACKAGE_VERSION" "$LATEST_RELEASE" | sort | tail -n1)" == "$LATEST_RELEASE" ]]; then
+      echo "Next predicted version must be higher than the latest release."
+      exit 1
+    fi
+  else
+    echo "PR branch must be release/v$PACKAGE_VERSION or hotfix/v$PACKAGE_VERSION"
+    exit 1
+  fi
+  if [[ ! "$PR_BRANCH" =~ ^release/v$PACKAGE_VERSION ]] && [[ ! "$PR_BRANCH" =~ ^hotfix/v$PACKAGE_VERSION ]]; then
+    echo "PR branch must be release/v$PACKAGE_VERSION or hotfix/v$PACKAGE_VERSION"
+    exit 1
+  fi
+fi
+
+echo "PR title and branch name validation passed."
---
+++ .github/actions/validate-pr/validate_pr_test.sh
@@ -0,0 +1,206 @@
+
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+function expect() {
+  if [ "$1" != "$2" ]; then
+    echo "Expected: $2"
+    echo "Actual:   $1"
+    exit 1
+  else
+    echo "OK"
+  fi
+}
+
+beforeAll() {
+  export TESTING=true
+}
+
+beforeEach() {
+  export PR_TITLE="feat: This is a PR title (ISSUE-1234)"
+  export PR_BRANCH="feature/ISSUE-1234"
+  export LATEST_RELEASE="1.0.0"
+  export PACKAGE_VERSION="1.1.0"
+  export TARGET_BRANCH="develop"
+}
+
+beforeAll
+
+echo Scenario: PR title missing semantic prefix for feature branch
+beforeEach
+
+# GIVEN
+export PR_TITLE="This is a PR title (ISSUE-1234)"
+
+# WHEN
+ACTUAL="$($SCRIPT_DIR/validate_pr.sh)"
+
+# THEN
+expect "$?" "1"
+expect "$ACTUAL" "PR title must start with a valid semantic prefix (e.g., feat:, fix:)."
+
+echo Scenario: PR title missing Jira ticket for feature branch
+beforeEach
+
+# GIVEN
+export PR_TITLE="feat: This is a PR title"
+
+# WHEN
+ACTUAL="$($SCRIPT_DIR/validate_pr.sh)"
+
+# THEN
+expect "$?" "1"
+expect "$ACTUAL" "PR title must contain a valid Jira ticket ID (e.g., ABC-123)."
+
+echo Scenario: PR title missing Jira ticket for feature branch targeting hotfix branch
+beforeEach
+
+# GIVEN
+export PR_TITLE="feat: This is a PR title"
+
+# WHEN
+ACTUAL="$($SCRIPT_DIR/validate_pr.sh)"
+
+# THEN
+expect "$?" "1"
+expect "$ACTUAL" "PR title must contain a valid Jira ticket ID (e.g., ABC-123)."
+
+echo Scenario: Valid PR title for feature branch
+beforeEach
+
+# GIVEN
+export PR_TITLE="feat: This is a PR title (ISSUE-1234)"
+
+# WHEN
+ACTUAL="$($SCRIPT_DIR/validate_pr.sh)"
+
+# THEN
+expect "$?" "0"
+expect "PR title and branch name validation passed." "$ACTUAL"
+
+echo Scenario: Non release or hotfix branch targeting the main branch
+beforeEach
+
+# GIVEN
+export PR_BRANCH="TICKET-123"
+export TARGET_BRANCH="main"
+
+# WHEN
+ACTUAL="$($SCRIPT_DIR/validate_pr.sh)"
+
+# THEN
+expect "$?" "1"
+expect "$ACTUAL" "PR branch must be release/v1.1.0 or hotfix/v1.1.0"
+
+echo Scenario: Package version and branch name mismatch
+beforeEach
+
+# GIVEN
+export PR_BRANCH="release/v1.1.0"
+export TARGET_BRANCH="main"
+export PACKAGE_VERSION="1.0.0"
+
+# WHEN
+ACTUAL="$($SCRIPT_DIR/validate_pr.sh)"
+
+# THEN
+expect "$?" "1"
+expect "$ACTUAL" "PR branch and package version must match"
+
+echo Scenario: Next Release version is not higher than the latest version
+beforeEach
+
+# GIVEN
+export PR_BRANCH="release/v1.0.0"
+export TARGET_BRANCH="main"
+export PACKAGE_VERSION="1.0.0"
+
+# WHEN
+ACTUAL="$($SCRIPT_DIR/validate_pr.sh)"
+
+# THEN
+expect "$?" "1"
+expect "$ACTUAL" "Next predicted version must be higher than the latest release."
+
+echo Scenario: Valid feature branch to develop branch
+beforeEach
+
+# GIVEN
+export PR_BRANCH="feature/ISSUE-1234"
+export TARGET_BRANCH="develop"
+
+# WHEN
+ACTUAL="$($SCRIPT_DIR/validate_pr.sh)"
+
+# THEN
+expect "$?" "0"
+
+echo Scenario: Valid feature branch to release branch
+beforeEach
+
+# GIVEN
+export PR_BRANCH="feature/ISSUE-1234"
+export TARGET_BRANCH="release/v1.1.0"
+
+# WHEN
+ACTUAL="$($SCRIPT_DIR/validate_pr.sh)"
+
+# THEN
+expect "$?" "0"
+
+echo Scenario: Valid feature branch to hotfix branch
+beforeEach
+
+# GIVEN
+export PR_BRANCH="feature/ISSUE-1234"
+export TARGET_BRANCH="hotfix/v1.1.0"
+
+# WHEN
+ACTUAL="$($SCRIPT_DIR/validate_pr.sh)"
+
+# THEN
+expect "$?" "0"
+
+echo Scenario: Valid release branch to main branch
+beforeEach
+
+# GIVEN
+export PR_BRANCH="release/v1.1.0"
+export TARGET_BRANCH="main"
+export PR_TITLE="release v1.1.0 to main"
+
+# WHEN
+ACTUAL="$($SCRIPT_DIR/validate_pr.sh)"
+
+# THEN
+expect "$?" "0"
+
+echo Scenario: Valid hotfix branch to main branch
+beforeEach
+
+# GIVEN
+export PR_BRANCH="hotfix/v1.1.0"
+export TARGET_BRANCH="main"
+export PR_TITLE="hotfix v1.1.0 to main"
+
+# WHEN
+ACTUAL="$($SCRIPT_DIR/validate_pr.sh)"
+
+# THEN
+expect "$?" "0"
+
+echo Scenario: Valid release branch merging to develop
+beforeEach
+
+# GIVEN
+export PR_BRANCH="release/v1.1.0"
+export TARGET_BRANCH="develop"
+export PR_TITLE="release v1.1.0 to develop"
+
+# WHEN
+ACTUAL="$($SCRIPT_DIR/validate_pr.sh)"
+
+# THEN
+expect "$?" "0"
+
