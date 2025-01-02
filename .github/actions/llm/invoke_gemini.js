4070fd5fa150658e7cb0487b3b4a1530943187cb
Ansel Robateau <ansel.robateau@redshelf.com>
2025-01-02 10:56:34 -0600

    fix: 🐛 add try catch block (#61)
    ✅ Closes: CORE-1736

--- .github/actions/llm/invoke_gemini.js
+++ .github/actions/llm/invoke_gemini.js
@@ -3,5 +3,9 @@ const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 const prompt = process.env.LLM_PROMPT;
 
-const result = await model.generateContent(prompt);
-console.log(result.response.text());
+try {
+  const result = await model.generateContent(prompt);
+  console.log(result.response.text());
+} catch (error) {
+  console.log('Next awesome release');
+}
