414485a8930698cec7ba837232315d277b08dc9c
Ansel Robateau <ansel.robateau@redshelf.com>
2025-04-04 16:16:31 -0500

    feat: 🎸 add layer dependency analysis javascript file
    ✅ Closes: PAR-2226

---
+++ .github/actions/static-analysis/layer_dependency_analysis.js
@@ -0,0 +1,10 @@
+class LayerDependencyAnalysis {
+    constructor() {}
+
+    run() {
+        console.log('Running layer dependency analysis');
+    }
+}
+
+const layerDependencyAnalysis = new LayerDependencyAnalysis();
+layerDependencyAnalysis.run();
